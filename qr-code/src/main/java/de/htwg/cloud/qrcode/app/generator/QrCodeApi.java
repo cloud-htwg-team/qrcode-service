6f84532cc55e2569ad6f41a1da890af4db010932
Adam Radvan <radvan@inqool.cz>
2023-01-18 17:11:08 +0100

    HTTP Request to History Microservice

--- qr-code/src/main/java/de/htwg/cloud/qrcode/app/generator/QrCodeApi.java
+++ qr-code/src/main/java/de/htwg/cloud/qrcode/app/generator/QrCodeApi.java
@@ -1,24 +1,34 @@
 package de.htwg.cloud.qrcode.app.generator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Slf4j
 @RestController
 @RequestMapping(QrCodeApi.QR_CODE_GENERATOR_REST_PATH)
 public class QrCodeApi {
 
     public static final String QR_CODE_GENERATOR_REST_PATH = "/qr-code";
+
     private final QrCodeService service;
 
     public QrCodeApi(QrCodeService service) {
@@ -26,21 +36,35 @@ public class QrCodeApi {
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
-    public ResponseEntity<InputStreamResource> generate(@RequestBody QrCodeApiDto dto) throws IOException {
-        ByteArrayOutputStream os = service.generate(dto.text);
+    public ResponseEntity<InputStreamResource> generate(@RequestBody QrCodeApiDto dto) throws IOException, URISyntaxException {
+        String textToEncode = dto.text;
+        if (textToEncode == null || textToEncode.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        ByteArrayOutputStream os = service.generate(textToEncode);
         int outputSize = os.size();
 
-        String name = "qr-code.png";
+        byte[] qrCodeBytes = os.toByteArray();
 
+        // Send Data to History Microservice
+        CompletableFuture<HttpResponse<String>> response = service.sendToHistoryServiceAsync(qrCodeBytes);
+        response.whenComplete((stringHttpResponse, throwable) -> log.info("Response received: {}", stringHttpResponse));
+
+
+        String name = "qr-code.png";
         String fileName = "filename=\"" + name + "\"";
         String fileNameAsterisk = "filename*=UTF-8''" + URLEncoder.encode(name, StandardCharsets.UTF_8);
 
-        return ResponseEntity.ok()
+        return ResponseEntity
+                .ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; " + fileName + "; " + fileNameAsterisk)
                 .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(outputSize))
                 .contentType(MediaType.IMAGE_PNG)
-                .body(new InputStreamResource(new ByteArrayInputStream(os.toByteArray())));
+                .body(new InputStreamResource(new ByteArrayInputStream(qrCodeBytes)));
     }
 
-    private record QrCodeApiDto(String text) {}
+
+    private record QrCodeApiDto(String text) {
+    }
 }
--- qr-code/src/main/java/de/htwg/cloud/qrcode/app/generator/QrCodeService.java
+++ qr-code/src/main/java/de/htwg/cloud/qrcode/app/generator/QrCodeService.java
@@ -1,18 +1,50 @@
 package de.htwg.cloud.qrcode.app.generator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.htwg.cloud.qrcode.app.lib.QrCodeLibraryUtil;
 import io.nayuki.qrcodegen.QrCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.concurrent.CompletableFuture;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @Component
 public class QrCodeService {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .findAndRegisterModules();
+    //            .configure(WRITE_DATES_AS_TIMESTAMPS, false); // timestamp number vs text
+    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+
+    private final String historyServiceServer;
+    private final String historyServicePort;
+    private final String historyServicePath;
+
+    public QrCodeService(@Value("${history.service.server}") String historyServiceServer,
+                         @Value("${history.service.port}") String historyServicePort,
+                         @Value("${history.service.path}") String historyServicePath) {
+        this.historyServiceServer = historyServiceServer;
+        this.historyServicePort = historyServicePort;
+        this.historyServicePath = historyServicePath;
+    }
+
 
     public ByteArrayOutputStream generate(String textToEncode) throws IOException {
         QrCode qrCode = QrCode.encodeText(textToEncode, QrCode.Ecc.HIGH);
@@ -23,6 +55,33 @@ public class QrCodeService {
         ImageIO.write(img, "png", os);
 
         return os;
+    }
+
+    public CompletableFuture<HttpResponse<String>> sendToHistoryServiceAsync(byte[] qrCodeBytes) throws URISyntaxException, JsonProcessingException {
+        URI historyServiceURI = new URI("http://%s:%s%s".formatted(
+                historyServiceServer,
+                historyServicePort,
+                historyServicePath
+        ));
+
+        String base64QrCode = Base64.getEncoder().encodeToString(qrCodeBytes);
+
+        DemoHistoryApi.HistoryDataDto historyDto = new DemoHistoryApi.HistoryDataDto(
+                "some-user-ID",
+                Instant.now(),
+                base64QrCode
+        );
+
+        String json = OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(historyDto);
+//        log.info(json);
+
+        HttpRequest historyServicePOSTRequest = HttpRequest.newBuilder()
+                .uri(historyServiceURI)
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        return HTTP_CLIENT.sendAsync(historyServicePOSTRequest, HttpResponse.BodyHandlers.ofString());
     }
 
 }
