7a76161a12509dcb6783b5ad8eb1fd5bdbd973e5
Adam Radvan <radvan@inqool.cz>
2023-01-26 12:06:05 +0100

    hotfix

--- qr-code/src/main/java/de/htwg/cloud/qrcode/QrcodeApplication.java
+++ qr-code/src/main/java/de/htwg/cloud/qrcode/QrcodeApplication.java
@@ -5,19 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static de.htwg.cloud.qrcode.app.generator.QrCodeApi.QR_CODE_GENERATOR_REST_PATH;
-
 @RestController
 @SpringBootApplication
 public class QrcodeApplication {
 
-	@GetMapping("/")
-	public String hello() {
-		return "QR microservice works! :)  - path: '/'";
-	}
+    @GetMapping("/")
+    public String hello() {
+        return "QR microservice works! :)  - path: '/'";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(QrcodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeApplication.class, args);
+    }
 
 }
